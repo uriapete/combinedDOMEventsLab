fe3e73f9f031dfb76f8480cff4be884d65084433
uriapete <uriarteangelo@gmail.com>
2023-02-25 07:41:46 -0800

    JS document nodes in converter.js

---
+++ converter.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("input");
+// console.log(input);
+
+const convertFrom = document.querySelector("#from")
+// console.log(convertFrom.value);
+
+const convertTo = document.getElementById("to")
+// console.log(convertTo);
+
+const submitButton = document.querySelector(".submit");
+// console.log(submitButton);
+
+const clearButton = document.querySelector(".clear");
+console.log(clearButton);
