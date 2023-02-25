487c47f321bc7fb8c9db9963886e7eee3f2c5dd9
uriapete <uriarteangelo@gmail.com>
2023-02-25 08:42:43 -0800

    MVP 001

--- converter.js
+++ converter.js
@@ -13,6 +13,8 @@ const submitButton = document.querySelector(".submit");
 const clearButton = document.querySelector(".clear");
 // console.log(clearButton);
 
+const output = document.querySelector(".output")
+
 let degrees;
 
 function convertTemperature() {
@@ -38,8 +40,20 @@ function convertTemperature() {
         // console.log(degrees);
     } else if (convertFrom.value === 'f' && convertTo.value === 'k'){
         degrees=((parsedInput-32)*5/9)+273.15
+    } else if (convertFrom.value = convertTo.value){
+        return output.innerHTML = "Converting into same unit!"
+    } else if(convertFrom.value === 'k' && convertTo.value === 'c') {
+        degrees = parsedInput - 273.15;
+    } else if(convertFrom.value === 'k' && convertTo.value === 'f') {
+        degrees = (parsedInput - 273.15) * 1.8 + 32;
     }
-    console.log(degrees);
+    // console.log(degrees);
+    output.innerHTML = `Converting ${parsedInput}°${convertFrom.value.toUpperCase()} into ${convertTo.value.toUpperCase()} would be ${degrees.toFixed(3)}°${convertTo.value.toUpperCase()}`;
+    if(parsedInput < 32) {
+        output.style.backgroundColor = 'blue';
+    } else if(parsedInput > 100) {
+        output.classList.add('boiling');
+    }
 }
 
 submitButton.addEventListener("click", convertTemperature)
