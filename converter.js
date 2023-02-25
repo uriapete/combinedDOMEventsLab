de4c130f8ce2854936636ff7633f45b058095a81
uriapete <uriarteangelo@gmail.com>
2023-02-25 08:18:35 -0800

    Basic converter

--- converter.js
+++ converter.js
@@ -11,4 +11,35 @@ const submitButton = document.querySelector(".submit");
 // console.log(submitButton);
 
 const clearButton = document.querySelector(".clear");
-console.log(clearButton);
+// console.log(clearButton);
+
+let degrees;
+
+function convertTemperature() {
+    // if (convertFrom.value === 'c' && convertTo==='f'){
+    //     degrees=input.value*1.8+32;
+    //     console.log(degrees);
+    // } else if (convertFrom.value==='f'&&convertTo.value==='c') {
+    //     degrees=(input.value/1.8)-32;
+    //     console.log(degrees);
+    // } else if(convertFrom.value === 'c' && convertTo.value === 'k') {
+    //     degrees=input.value+273.15;
+    //     console.log(degrees);
+    // }
+    let parsedInput = parseInt(input.value)
+    if(convertFrom.value === 'c' && convertTo.value === 'f') {
+        degrees = parsedInput * 1.8 + 32;
+        // console.log(degrees);
+    } else if(convertFrom.value === 'f' && convertTo.value == 'c') {
+        degrees = (parsedInput - 32) / 1.8;
+        // console.log(degrees);
+    } else if(convertFrom.value === 'c' && convertTo.value === 'k') {
+        degrees = parsedInput + 273.15;
+        // console.log(degrees);
+    } else if (convertFrom.value === 'f' && convertTo.value === 'k'){
+        degrees=((parsedInput-32)*5/9)+273.15
+    }
+    console.log(degrees);
+}
+
+submitButton.addEventListener("click", convertTemperature)
